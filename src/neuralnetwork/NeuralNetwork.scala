051db552b51704dc3668f7c653696773c0c0ddc5
jessehartloff <hartloff@buffalo.edu>
2020-03-17 15:55:51 -0400

    Move NeuralNetwork numberOfDimensions to the object instead of the class

--- src/neuralnetwork/NeuralNetwork.scala
+++ src/neuralnetwork/NeuralNetwork.scala
@@ -10,23 +10,16 @@ object NeuralNetwork{
     null
   }
 
+  def numberOfDimensions(numberOfInputNodes: Int, numberOfCategories: Int): Int = {
+    0
+  }
 }
 
 
-class NeuralNetwork(numberOfInputNodes: Int, outputCategories: List[String]) {
+class NeuralNetwork(numberOfInputNodes: Int, outputCategories: List[String], genes: List[Double]) {
 
   def findCategory(inputs: List[Double]): String = {
     ""
-  }
-
-
-  def setParameters(parameters: List[Double]): Unit = {
-
-  }
-
-
-  def numberOfDimensions(): Int = {
-    0
   }
 
 }
